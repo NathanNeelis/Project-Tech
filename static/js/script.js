c2d9a7e85e720b8533545d50b6a009c146761820
NathanNeelis <nathan.neelis@hva.nl>
2020-06-10 21:41:20 +0200

    PE update and some wiki images about PE

--- static/js/script.js
+++ static/js/script.js
@@ -12,8 +12,27 @@ function handleFiles() {
         "Your upload: " + fileList[0].name;
 }
 
-
-
 // Resource filetext after uploading.
 // https://stackoverflow.com/questions/16505333/get-the-data-of-uploaded-file-in-javascript
 // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+
+
+//Progressive enhancement toggle ----------------------------------------------
+const validateRegister = document.querySelector("#profilePicture");
+let pictureFeedbackFile = document.querySelector("#profilePicture");
+let pictureFeedbackLabel = document.querySelector("#picLabel");
+
+if (validateRegister) {
+    console.log("Validated!");
+    pictureFeedbackFile.classList.add("hideWebLayout");
+    pictureFeedbackLabel.classList.add("pictureLabel");
+    pictureFeedbackLabel.classList.add("pictureLabel:hover");
+} else {
+    console.log("not validated!");
+    pictureFeedbackFile.classList.remove("hideWebLayout");
+    pictureFeedbackLabel.classList.remove("pictureLabel");
+    pictureFeedbackLabel.classList.remove("pictureLabel:hover");
+
+}
+
+// resource Victor B - tech 4 
